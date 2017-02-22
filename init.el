928d85b71d83c41cfe39fc0c8b6fb97832edb3f6
unfor9otten <unfor9otten@gmail.com>
2017-02-23 07:34:21 +0900

    helm init config setting  modified

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;; 한글
+(set-language-environment "Korean")
+
 (prefer-coding-system 'utf-8)
 
 (setq package-archives
@@ -35,3 +38,17 @@
    (ufn-load-org "org")
    (ufn-load-org "helm")
    ))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (helm-ag helm-swoop helm-projectile helm org-pomodoro org-fstree ox-gfm company-flx company paredit feature-mode yaml-mode atom-one-dark-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
