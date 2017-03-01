3a687063dcd5dea888e3ae1e1fda16c4c819d81e
unfor9otten <unfor9otten@gmail.com>
2017-03-01 21:58:16 +0900

    [add] extension file load

--- init.el
+++ init.el
@@ -1,12 +1,18 @@
-;; 한글
-(set-language-environment "utf-8")
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/System-Environment.html
+;; system-type settings
+(defconst *is-windows* (eq system-type 'windows-nt))
+(defconst *is-linux* (eq system-type 'gnu/linux))
+(defconst *doc-home-path* "~/doc-home")
+
+(when *is-linux*
+  (set-language-environment "utf-8"))
 
 (prefer-coding-system 'utf-8)
 
 (setq package-archives
-      '(
-	("gnu" . "https://elpa.gnu.org/packages/")
-	("melpa" . "https://melpa.org/packages/")))
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
+
 
 (package-initialize)
 (unless (file-exists-p "~/.emacs.d/elpa/archives/melpa") (package-refresh-contents))
@@ -14,14 +20,6 @@
 (package-install 'use-package)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(add-to-list 'load-path (expand-file-name "init.d" user-emacs-directory))
-
-;; https://www.gnu.org/software/emacs/manual/html_node/elisp/System-Environment.html
-;; system-type settings
-(defconst *is-windows* (eq system-type 'windows-nt))
-(defconst *is-linux* (eq system-type 'gnu/linux))
-
 
 (require 'org-install)
 (require 'ob-tangle)
@@ -31,11 +29,23 @@
 
 (defun ufn-load-org (s)
   (org-babel-load-file
-   (expand-file-name (format "init.d/init-%s.org" s) ufn-config-dir)))
+   (expand-file-name (format "%s.org" s) ufn-config-dir)))
+
+(defun ufn-init-load-org (s)
+  (ufn-load-org (format "init.d/init-%s" s)))
+
+;; private extension load
+(defvar private-extension-file (concat *doc-home-path* "/private/extension-load"))
 
 (add-hook
  'after-init-hook
  (lambda ()
-   (ufn-load-org "emacs-customize")
-   (ufn-load-org "org")
-   (ufn-load-org "helm")))
+   (ufn-init-load-org "emacs-customize")
+   (ufn-init-load-org "org")
+   (ufn-init-load-org "helm")
+
+
+   (when (file-exists-p (concat private-extension-file ".org"))
+     (ufn-load-org private-extension-file))
+   )
+ )
